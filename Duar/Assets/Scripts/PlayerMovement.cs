94e94fe4a2ec95d3029b4d4aee97338cbec86ac0
Arcantica <marcelinokevinncb@gmail.com>
2021-10-05 16:39:49 +0700

    platform update
    [fix] idle animation
    [fix] left right animation
    [add] new platform model

--- Duar/Assets/Scripts/PlayerMovement.cs
+++ Duar/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    public float jumpSpeed = 5f;
-    Vector2 movement;
+    public GameObject gameOver;
+    public float moveSpeed = 1f;
+    public float jumpSpeed = 1;
     public vectorValue startingPosition;
     public Animator animator;
-    private CapsuleCollider2D box;
+    Vector2 move;
     [SerializeField] private LayerMask layerMask;
 
     void FixedUpdate()
@@ -18,56 +18,31 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    public GameObject gameOver;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        box = transform.GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-        if (Input.GetKey("space") && IsGrounded())
+        var movement = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Horizontal", movement);
+        if (movement == 0)
         {
-            float jumpVelocity = jumpSpeed;
-            rb.velocity = Vector2.up * jumpVelocity;
+            animator.SetBool("isMoving", false);
         }
-        Movement();
-
-        if (IsGrounded())
+        else
         {
-            if(rb.velocity.x != 0)
-            {
-                animator.SetFloat("Horizontal", rb.velocity.x);
-                animator.SetFloat("Speed", rb.velocity.sqrMagnitude);
-            } else
-            {
-                animator.SetFloat("Horizontal", rb.velocity.x * 0);
-                animator.SetFloat("Speed", rb.velocity.sqrMagnitude * 0);
-            }
+            animator.SetBool("isMoving", true);
         }
-    }
+        transform.position = transform.position + new Vector3(movement * moveSpeed * Time.deltaTime, 0, 0);
 
-    private bool IsGrounded()
-    {
-        RaycastHit2D raycast = Physics2D.BoxCast(box.bounds.center, box.bounds.size, 0f, Vector2.down, .1f, layerMask);
-        return raycast.collider != null;
-    }
-    private void Movement()
-    {
-        float speed = moveSpeed;
-        if(Input.GetKey("left"))
+        if (Input.GetKeyDown(KeyCode.W) && Mathf.Abs(rb.velocity.y) < 1)
         {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        } else
-        {
-            if(Input.GetKey("right"))
-            {
-                rb.velocity = new Vector2(+speed, rb.velocity.y);
-            } else { }
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision2D)
@@ -78,6 +53,5 @@ public class PlayerMovement : MonoBehaviour
             gameOver.SetActive(true);
         }
     }
-
     
 }
---
+++ Duar/Assets/Scripts/oldPlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oldPlayerMovement : MonoBehaviour
+ {
+    public Rigidbody2D rb;
+    public GameObject gameOver;
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    public vectorValue startingPosition;
+    public Animator animator;
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.tag == "Enemy")
+        {
+            Collider2D.Destroy(gameObject);
+            gameOver.SetActive(true);
+        }
+    }
+
+
+}
+
+
