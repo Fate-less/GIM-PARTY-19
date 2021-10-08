be14768ea307dd84fd462ba5b29bef42d26bfa64
Arcantica <marcelinokevinncb@gmail.com>
2021-10-08 15:28:37 +0700

    Tilemap Update
    [+] Assets

---
+++ Duar/Assets/Scripts/toDark.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toDark : MonoBehaviour
+{
+    public Vector2 playerPosition;
+    public vectorValue playerStorage;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        playerStorage.initialValue = playerPosition;
+        SceneManager.LoadScene(1);
+        Debug.Log("Player Teleported, Next Scene");
+    }
+}
---
+++ Duar/Assets/Scripts/toTileventure.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toTileventure : MonoBehaviour
+{
+    public Vector2 playerPosition;
+    public vectorValue playerStorage;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        playerStorage.initialValue = playerPosition;
+        SceneManager.LoadScene(2);
+        Debug.Log("Player Teleported, Next Scene");
+    }
+}
